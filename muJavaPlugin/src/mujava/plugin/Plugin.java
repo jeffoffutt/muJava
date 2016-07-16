43ba3d05e34e833983a16ca210d1fcbb0b7a759d
Shreyash N. Paliwal <snp59@njit.edu>
2016-07-15 21:23:02 -0400

    updated the project as per Nafi's directions.

--- muJavaPlugin/src/mujava/plugin/Plugin.java
+++ muJavaPlugin/src/mujava/plugin/Plugin.java
@@ -7,262 +7,317 @@ import static mujava.cli.testnew.muJavaHomePath;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileFilter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 
-import com.beust.jcommander.JCommander;
-
-import mujava.MutationSystem;
-import mujava.cli.Util;
-import mujava.cli.genmutesCom;
 import mujava.cli.testnew;
-import mujava.cli.testnewCom;
 import mujava.cli.genmutes;
+import mujava.cli.runmutes;
 
+/**
+ * @author Shreyash
+ */
 public class Plugin {
+
+	private static String operatorString;
+	private static String baseProgram;
+	private static String testOracle;
+	private static File combinedMutantFile;
+	private static final String COMBINED_MUTANTS_FILE_NAME = "CombinedMutants";
+	
+	private static List<String> methodList = new ArrayList<String>();
+
+	private static void parseArgs(String[] args) {
+
+		if (args == null || args.length != 4) {
+			throw new IllegalArgumentException("Please provide session name and list of mutation operators as argument."
+					+ "Example -> session=session1 operator=-AORB basep=C:\\Users\\Shreyash"
+					+ "\\Desktop\\cal.java oracle=C:\\Users\\Shreyash\\Desktop\\TestCal.class");
+		}
+
+		for (String arg : args) {
+			String[] tempArg = arg.split("=");
+
+			switch (tempArg[0]) {
+			case "session":
+				sessionName = tempArg[1];
+				break;
+			case "operator":
+				operatorString = tempArg[1];
+				break;
+			case "basep":
+				baseProgram = tempArg[1];
+				break;
+			case "oracle":
+				testOracle = tempArg[1];
+				break;
+			default:
+				throw new IllegalArgumentException(
+						"Please provide session name and list of mutation operators as argument."
+								+ "Example -> session=session1 operator=-AORB basep=C:\\Users\\Shreyash"
+								+ "\\Desktop\\cal.java oracle=C:\\Users\\Shreyash\\Desktop\\TestCal.class");
+			}
+		}
+
+		System.out.println("Session Name: " + sessionName);
+		System.out.println("Operator Name: " + operatorString);
+		System.out.println("Base Program Name: " + baseProgram);
+
+		muJavaHomePath = System.getProperty("user.dir") + separator + "src" + separator + "mujava";
+		mujava.cli.genmutes.muJavaHomePath = muJavaHomePath;
+		mujava.cli.runmutes.muJavaHomePath = muJavaHomePath;
+	}
+
+	private static void createTestSession() {
+
+		String[] argTestNew = { sessionName, baseProgram };
+		try {
+			testnew.main(argTestNew);
+		} catch (IOException e1) {
+
+			System.err.println("Error while creating test session!");
+			e1.printStackTrace();
+			System.exit(1);
+		}
+
+	}
+
+	private static void generateMutants() {
+
+		String[] argGenMutes = { operatorString, sessionName };
+
+		try {
+			genmutes.main(argGenMutes);
+		} catch (Exception e1) {
+			System.err.println("Error while generating mutants!");
+			e1.printStackTrace();
+			System.exit(1);
+		}
+
+	}
+
+	private static void runTests() {
+
+		String testOracleName = testOracle.substring(testOracle.lastIndexOf(separator) + 1,
+				testOracle.length() - ".class".length());
+
+		File oracleFile = new File(testOracle);
+		File testsetDir = new File(muJavaHomePath + "/" + sessionName + "/testset");
+		try {
+
+			FileUtils.copyFileToDirectory(oracleFile, testsetDir);
+		} catch (IOException e) {
+			System.err.println("Error while copying oracle to testset directory!");
+			e.printStackTrace();
+		}
+
+		String[] argRunTest = { testOracleName, sessionName };
+
+		try {
+			runmutes.main(argRunTest);
+		} catch (Exception e1) {
+			System.err.println("Error while running tests!");
+			e1.printStackTrace();
+			System.exit(1);
+		}
+
+	}
 
 	public static void main(String[] args) {
 
-		if (args == null || args.length < 2) {
-			new IllegalArgumentException("Please provide session name and list of mutation operators as argument.");
-		}
-				
-		sessionName = args[0];
-		muJavaHomePath = System.getProperty("user.dir") + separator + "src" + separator + "mujava";
-		mujava.cli.genmutes.muJavaHomePath = muJavaHomePath;
+		parseArgs(args);
+
+		createTestSession();
 		
-		// Source directory
-		File sourceDir = new File(muJavaHomePath + separator + "source");
+		generateMutants();
+		
+		//runTests();
 
-		// Create source directory if it does not exist.
-		if (!sourceDir.exists()) {
+		combineMutants();
+	
+	}
 
-			sourceDir.mkdir();
+	public static void combineMutants() {
 
-			throw new RuntimeException("ERROR: No source package found. \n"
-					+ "A new source package has been created with the name mujava.source. \n"
-					+ "Please put all the java source files in this package."
-					+ "NOTE: There should be only one file with main() method.");
+		combinedMutantFile = new File(muJavaHomePath + separator + sessionName + separator + "result" + separator
+				+ COMBINED_MUTANTS_FILE_NAME + ".java");
 
+		try {
+			combinedMutantFile.createNewFile();
+		} catch (IOException e2) {
+			e2.printStackTrace();
 		}
 
-		// Session directory
-		File sessionDir = new File(
-				System.getProperty("user.dir") + separator + "src" + separator + "mujava" + separator + sessionName);
-
-		// Make sure session directory does not exist.
-		if (sessionDir.exists()) {
-
-			new RuntimeException("Session directory exists!!! \n" + "Please make sure session name is not reused.");
-
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(combinedMutantFile);
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
+		
+		File[] resultSetBaseDirectory = new File(
+				muJavaHomePath + separator + sessionName + separator + "result" + separator
+						+ baseProgram.substring(baseProgram.lastIndexOf(separator) + 1, baseProgram.lastIndexOf("."))
+						+ separator + "traditional_mutants").listFiles();
 
-		// accept java files in the mujava.source folder
-		List<File> sourceFiles = Arrays.asList(sourceDir.listFiles(new FileFilter() {
-			@Override
-			public boolean accept(File pathname) {
-				if (pathname != null && pathname.getName().toLowerCase().endsWith(".java")) {
-					return true;
-				}
-				return false;
+		String methodSignature = null;
+
+		List<File> resultSetMethodDirectory = new ArrayList<File>();
+
+		for (File file : resultSetBaseDirectory) {
+			if (file.isDirectory()) {
+				resultSetMethodDirectory.add(file);
+
+				String filename = file.getName();
+				methodSignature = filename.substring(filename.lastIndexOf("_") + 1, filename.indexOf("("));
 			}
-		}));
+		}
 
-		String[] argv = new String[sourceFiles.size() + 1];
-		
-		argv[0] = args[0];
+		List<File> resultSetMutantsDirectory = new ArrayList<File>();
 
-		int argsCounter = 1;
+		for (File file : resultSetMethodDirectory) {
+			if (file.isDirectory())
+				resultSetMutantsDirectory.addAll(Arrays.asList(file.listFiles()));
+		}
 
-		// create sub-directory for the session
-		testnew.setupSessionDirectory(sessionName);
+		for (File mutantDirectory : resultSetMutantsDirectory) {
+			System.out.println(mutantDirectory);
+		}
 
-		// Get all java source files under package mujava.source
-		for (File file : sourceFiles) {
-			System.out.println("Found source file - " + file.getName());
+		boolean importsExtracted = false;
 
-			argv[argsCounter] = file.getAbsolutePath();
-			argsCounter++;
+		for (File mutantDirectory : resultSetMutantsDirectory) {
 
-			// copy to session directory
+			File mutantFile = mutantDirectory.listFiles(new FilenameFilter() {
+
+				@Override
+				public boolean accept(File dir, String name) {
+					if (name.endsWith(".java"))
+						return true;
+					return false;
+				}
+			})[0];
+
+			System.out.println("Mutant found -" + mutantDirectory.getAbsolutePath() + separator + mutantFile.getName());
+
+			if (!importsExtracted) {
+				BufferedReader reader = null;
+				try {
+					reader = new BufferedReader(new FileReader(mutantFile));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+
+				try {
+					String temp = reader.readLine();
+					while (temp != null) {
+						if (temp.contains("import")) {
+							writer.append(temp + "\n");
+						}
+						temp = reader.readLine();
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				try {
+					writer.append("public class " + COMBINED_MUTANTS_FILE_NAME + "{ \n");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				importsExtracted = true;
+			}
+
 			try {
+				writer.append(extractMethod(mutantFile, methodSignature));
+			} catch (IOException e) {
+				System.err.println("Error while extracting method from mutant " + mutantDirectory.getAbsolutePath()
+						+ separator + mutantFile.getName());
+				e.printStackTrace();
+			}
+		}
+		try {
+			writer.append("}");
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-				File sessionSrcDir = new File(sessionDir.getAbsolutePath() + separator + "src");
-				FileUtils.copyFileToDirectory(file, sessionSrcDir);
+	private static String extractMethod(File mutantFile, String methodSignature) {
+		StringBuffer method = new StringBuffer();
 
-				updateJavaPackageName(new File(sessionSrcDir + separator + file.getName()), sessionName);
+		int codeBlockStart = 0;
+		int codeBlockEnd = 0;
 
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(mutantFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		boolean methodFound = false;
+		String temp = null;
+		while (!methodFound) {
+			try {
+				temp = reader.readLine();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
-
-			System.out.println("argv: "+argv);
-			
-			System.out.println(sessionDir.getAbsolutePath() + separator + "src" + separator
-					+ file.getName().substring(0, file.getName().length() - ".java".length()));
-			boolean result = testnew.compileSrc(sessionDir.getAbsolutePath() + separator + "src" + separator
-					+ file.getName().substring(0, file.getName().length() - ".java".length()));
-			System.out.println(result);
-			if (result)
-				Util.Print("Session is built successfully.");
+			if (temp.contains(methodSignature))
+				methodFound = true;
 		}
 
-		testnewCom testNewJct = new testnewCom();
+		String newMethodSignature = methodSignature + "_" + mutantFile.getParentFile().getName();
 
-		if (testNewJct.isDebug() || testNewJct.isDebugMode()) {
-			Util.debug = true;
-		}
+		methodList.add(newMethodSignature);
 
-		new JCommander(testNewJct, argv);
+		method.append(temp.replace(methodSignature, newMethodSignature) + "\n");
 
-		genmutesCom genMutesJct = new genmutesCom();
-		
-		// get all mutation operators selected
-				HashMap<String, List<String>> ops = new HashMap<String, List<String>>();
+		if (temp.contains("{"))
+			codeBlockStart++;
+		else if (temp.contains("}"))
+			codeBlockEnd++;
 
-		
-		JCommander jCommander = new JCommander(genMutesJct, args);
-
-		
-		String[] paras = new String[] { "1", "0" };
-		if (genMutesJct.getAll()) // all is selected, add all operators
-		{
-
-			// if all is selected, all mutation operators are added
-			ops.put("AORB", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AORS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AOIU", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AOIS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AODU", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AODS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("ROR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("COR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("COD", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("COI", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("SOR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("LOR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("LOI", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("LOD", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("ASRS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("SDL", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("ODL", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("VDL", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("CDL", new ArrayList<String>(Arrays.asList(paras)));
-			// ops.put("SDL", genMutesJct.getAll());
-
-		} else { // if not all, add selected ops to the list
-			if (genMutesJct.getAORB()) {
-				ops.put("AORB", new ArrayList<String>(Arrays.asList(paras)));
+		while ((codeBlockStart == 0 && codeBlockEnd == 0) || codeBlockStart != codeBlockEnd) {
+			try {
+				temp = reader.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			if (genMutesJct.getAORS()) {
-				ops.put("AORS", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getAOIU()) {
-				ops.put("AOIU", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getAOIS()) {
-				ops.put("AOIS", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getAODU()) {
-				ops.put("AODU", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getAODS()) {
-				ops.put("AODS", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getROR()) {
-				ops.put("ROR", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getCOR()) {
-				ops.put("COR", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getCOD()) {
-				ops.put("COD", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getCOI()) {
-				ops.put("COI", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getSOR()) {
-				ops.put("SOR", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getLOR()) {
-				ops.put("LOR", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getLOI()) {
-				ops.put("LOI", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getLOD()) {
-				ops.put("LOD", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getASRS()) {
-				ops.put("ASRS", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getSDL()) {
-				ops.put("SDL", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getVDL()) {
-				ops.put("VDL", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getODL()) {
-				ops.put("ODL", new ArrayList<String>(Arrays.asList(paras)));
-			}
-			if (genMutesJct.getCDL()) {
-				ops.put("CDL", new ArrayList<String>(Arrays.asList(paras)));
+			if (temp == null) {
+				break;
+			} else {
+				method.append(temp + "\n");
+				if (temp.contains("{"))
+					codeBlockStart++;
+				else if (temp.contains("}"))
+					codeBlockEnd++;
 			}
 		}
 
-		// add default option "all"
-		if (ops.size() == 0) {
-			ops.put("AORB", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AORS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AOIU", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AOIS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AODU", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("AODS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("ROR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("COR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("COD", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("COI", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("SOR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("LOR", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("LOI", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("LOD", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("ASRS", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("SDL", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("ODL", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("VDL", new ArrayList<String>(Arrays.asList(paras)));
-			ops.put("CDL", new ArrayList<String>(Arrays.asList(paras)));
+		System.out.println(method.toString());
+
+		if (reader != null) {
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 
+		return method.toString();
 
-		// String[] tradional_ops = ops.toArray(new String[0]);
-		// set system
-		genmutes.setJMutationStructureAndSession(sessionName);
-		// MutationSystem.setJMutationStructureAndSession(sessionName);
-		try {
-			MutationSystem.recordInheritanceRelation();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// generate mutants
-		
-		String[] fileNames = new String[sourceFiles.size()];
-		int i=0;
-		for(File file:sourceFiles){
-			fileNames[i]=file.getName();
-			i++;
-		}
-		
-		genmutes.generateMutants(fileNames, ops);
 	}
 
 	public static void updateJavaPackageName(File javaFile, String sessionName) throws IOException {
@@ -277,8 +332,8 @@ public class Plugin {
 			String line;
 			while ((line = br.readLine()) != null) {
 				if (line.contains("package"))
-					//line = line.replace("mujava.source", "mujava." + sessionName + ".src");
-					line="";
+					line = line.replace("mujava.source", "mujava." + sessionName + ".src");
+				// line="";
 				bw.write(line + "\n");
 			}
 		} finally {
@@ -297,9 +352,38 @@ public class Plugin {
 		newFile.renameTo(javaFile);
 
 	}
-	/*
-	 * String[] argv = { "-all", "-debug", "Flower" }; genmutesCom jct = new
-	 * genmutesCom();
-	 */
+
+	public static void addJavaPackageName(File javaFile, String sessionName) throws IOException {
+
+		String tmpFileName = "tmp_try.dat";
+
+		BufferedReader br = null;
+		BufferedWriter bw = null;
+		try {
+			br = new BufferedReader(new FileReader(javaFile));
+			bw = new BufferedWriter(new FileWriter(tmpFileName));
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (line.contains("package"))
+					line = line.replace("mujava.source", "mujava." + sessionName + ".src");
+				line = "";
+				bw.write(line + "\n");
+			}
+		} finally {
+			if (br != null)
+				br.close();
+
+			if (bw != null)
+				bw.close();
+		}
+		// Once everything is complete, delete old file..
+
+		javaFile.delete();
+
+		// And rename tmp file's name to old file name
+		File newFile = new File(tmpFileName);
+		newFile.renameTo(javaFile);
+
+	}
 
 }
